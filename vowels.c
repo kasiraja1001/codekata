6318df5c47ffc81553d020a5a1ba9d0aedf86681
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-05-20 14:20:56 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+char ch;
+scanf("%s",&ch);
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='z'))
+{
+    if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'|| ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+    {
+        printf("Vowel");
+    }
+else
+{
+    printf("Consonent");}}
+else
+{
+    printf("Invalid");
+}
+}
