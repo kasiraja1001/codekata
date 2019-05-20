6c8e601c849d6ea3789c903d0139c4338bc4195c
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-05-20 15:03:14 +0530

    Update bignum.c

--- bignum.c
+++ bignum.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-main()
+int main()
 {
 int a,b,c;
-scanf("%d %d %d",&a,&b,&c);
+scanf("%d%d%d",&a,&b,&c);
 if((a>b)&&(a>c))
 {
- printf("%d",a);
+printf("%d",a);
 }
-else if((b>a)&&(b>c))  
+else if((b>a)&&(b>c))
 {
 printf("%d",b);
 }
 else
 {
 printf("%d",c);
+}
 }
