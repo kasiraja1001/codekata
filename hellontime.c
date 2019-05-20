522878b1fb90e8fabb21d02251ba0af9b4c828fc
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-05-20 19:18:58 +0530

    Create hellontime.c

---
+++ hellontime.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   int a,i;
+   scanf("%d",&a);
+  if(a>0)
+   {
+        for(i=0;i<a;i++)
+       {
+       printf("Hello \n");
+      }
+   }
+   else
+   {
+       return 0;
+      
+   }
+   return 0;
+}
