6911e78ad50292931d8d50f6efc4f3ac8430b2e9
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-05-22 20:28:13 +0530

    Create arraypos.c

---
+++ arraypos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,a,max,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        max=arr[0];
+    }
+    for(i=1;i<n;i++)
+    if(max<arr[i])
+    {
+        max=arr[i];
+    }
+    printf("%d ",max);
+    
+}
