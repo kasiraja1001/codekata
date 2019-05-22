fbf2785e9f4de8353bf128c60407b41c130b191f
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-05-22 21:49:10 +0530

    Update median.c

--- median.c
+++ median.c
@@ -1,24 +1,24 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+
+int main()
 {
-    int n,a[1000],i,temp=0,j,med=0;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-      for(i=0;i<n;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if(a[i]>a[j])
-            {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
-    med=(n/2);
-        printf("%d",a[med]);
-    }
+   int i,j,a[20],n,temp=0,m=0;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+       scanf("%d",&a[i]);
+       for(i=0;i<n;i++)
+       {
+       for(j=i+1;j<n;j++)
+       {
+       if(a[i]>a[j])
+       {
+           temp=a[i];
+           a[i]=a[j];
+           a[j]=temp;
+       }
+       }
+       }
+       
+       m=(n/2);  
+           printf("%d ",a[m]);
+}
