e5e840b5f3c9c74387f1d0d3d1a21f151dc33dfa
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-05-23 14:18:03 +0530

    Create splchar.c

---
+++ splchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char s[15000];
+    int i=0,n,c=0;
+    gets(s);
+    while(s[i]!='\0')
+    {
+        if((s[i]>=33&&s[i]<=47)||(s[i]>=58&&s[i]<=64)||(s[i]>=91&&s[i]<96)||(s[i]>=123&&s[i]<=126))
+    c++;
+    i++;
+    }
+    }
+    printf("%d",c);
+}
