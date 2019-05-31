d34a36832bda2188ec867e2d919eb52cb42cd6ac
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-05-31 13:40:06 +0530

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,fact;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+         if(n%i==0) 
+         {
+             fact=i;
+         }
+    }
+    if(fact>1) 
+    {
+          printf ("yes");
+    }
+    else 
+    {
+          printf ("no");
+    }
+    return 0;
+}
