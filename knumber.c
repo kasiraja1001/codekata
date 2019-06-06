a3cb4ddf96694dbd16c0bba4aeac66f8acc8c22d
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-06-06 13:35:46 +0530

    Create knumber.c

---
+++ knumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,m,i,a[10],temp;
+scanf("%d%d",&n,&m);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+if(a[i]==m)
+temp==1;
+}
+printf("%d",m);
+return 0;
+}
