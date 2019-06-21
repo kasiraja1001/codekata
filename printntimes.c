cd998684c1892a75b6612d33b1b2830f296a570f
KASI RAJA PANDIAN C <41238556+kasiraja1001@users.noreply.github.com>
2019-06-21 19:27:45 +0530

    Create printntimes.c

---
+++ printntimes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+     char str[20];
+     int a,i;
+     scanf("%s %d",str,&a);
+     for(i=0;i<a;i++)
+     {
+        printf("%s\n",str); 
+     }
+     return 0;
+}
